a00218c1cac26399a4a23d839400a5e49ae26408
Rojda Polat <rojdapolat@outlook.de>
2025-03-11 17:58:12 +0100

    day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+**JSON Dataset - Recipes**
+
+This Gist contains a JSON dataset of recipes for a recipe book app.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import recipesLogo from "../assets/recipe-logo.jpg";
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={recipesLogo}/>
+    </nav>
+  )
+}
+
+export default Navbar
