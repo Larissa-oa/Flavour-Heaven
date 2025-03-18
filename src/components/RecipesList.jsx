a047e2803bccd1c3c6b1a89dc8d97ef0addb24f6
Larissa Almeida <oa.larissa@hotmail.com>
2025-03-18 20:48:54 +0100

    new

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import './RecipesList.css'
 
 const RecipesList = ({ recipes, addToFavourites, onUpdate }) => {
   // State for sort type
